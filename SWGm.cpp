ce26eb40250fc783bf257ad6b6ae79a0cb946ce3
franadam <franadam@users.noreply.github.com>
2015-11-30 12:30:33 +0100

    Create SWGm.cpp

---
+++ SWGm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <getopt.h>
+#include <utility>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+class SWGm{
+public:
+  //Constructor
+    SWGm();
+  //Destructor  
+    SWGm(void);
+private:
+  //create the matrix
+  void CreateMatrix(pReference, pQuery)
+  ;}
