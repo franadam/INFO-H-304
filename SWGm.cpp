12fb3e267d4ff0eab15d8296108a3e8c6baffff6
franadam <franadam@users.noreply.github.com>
2015-11-30 14:02:37 +0100

    Update SWGm.cpp

--- SWGm.cpp
+++ SWGm.cpp
@@ -16,6 +16,46 @@ public:
   //Destructor  
     SWGm(void);
 private:
+
+  //initialization
+  int matchScore = 0;
+  int mismatchScore = 1;
+  int gapOpenPenalty = 11;
+  int gapExtendPenalty = 1;
+  int t[][]=NULL;
+  
+  char pReference = "ATGGCGGGGATCGGGACACTCGCCGGTGCGGGTACCCTA"
+  char pQuery = "GGGGATCGGGACACTCGCTCTCCGGTGCGGGTA"
+  
+  int n = strlen(pReference);
+	int m = strlen(pQuery);
+  
+  //gap penalty
+  int CalculateGapWeight(gapOpenPenalty, gapExtendPenalty,int k){
+   int w_k=gapOpenPenalty + gapExtendPenalty*k
+   return k
+  }
+  
   //create the matrix
-  void CreateMatrix(pReference, pQuery)
-  ;}
+  int CreateMatrix(pReference, pQuery){
+    for(unsigned char i = 0; i < n; i++) {
+		  for(unsigned char j = 0; j < m; j++) {
+		    t[i][j]=0;
+		  }
+    }
+    return t;
+  }
+  
+  int InitMat
+  int D = t ;
+  int P = t ;
+  int Q = t ;
+  for(unsigned char i = 0; i < strlen(pReference); i++) {
+    D[i][0] = CalculateGapWeight(gapOpenPenalty, gapExtendPenalty,int i)
+    Q[i][0] = 2000
+  }
+	for(unsigned char j = 0; j < strlen(pQuery); j++) {
+	  D[0][j] = CalculateGapWeight(gapOpenPenalty, gapExtendPenalty,int j)
+	  P[0][j] = 2000
+	}
+}
