6db3078b5ca99f4ec8ab321066cd75fa57cc9be1
franadam <franadam@users.noreply.github.com>
2015-12-01 12:26:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <ostream>
+//#include <ostream>
 #include <stdio.h>
 #include <string.h>
 #include <fstream>
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 {
 	//terminal  g++ -o hello hello.cpp puis ./hello
 
-	int matchScore = 0;
-	int mismatchScore = 1;
+	//int matchScore = 0;
+	//int mismatchScore = 1;
   int gapOpenPenalty = 11;
   int gapExtendPenalty = 1;
 	  
@@ -33,18 +33,30 @@ int main(int argc, char *argv[])
   int CalculateGapWeight(int gapOpenPenalty, int gapExtendPenalty,int k);
   void InitMat(int** D, int** P, int**Q,int gapOpenPenalty, int gapExtendPenalty);
   int getSizeString(string a);
-  void CalculateMatrixD(int ** D,int ** P, int ** Q,string pReference, string pQuery);
+ int ** CalculateMatrixD(int ** D,int ** P, int ** Q,string pReference, string pQuery);
+ int ** CalculateMatrixP(int ** D, int ** P, int ** Q, string pReference, string pQuery);
+ int ** CalculateMatrixQ(int ** D, int ** P, int ** Q, string pReference, string pQuery);
+   void CalculateAllMatrix(int ** D, int ** P, int ** Q, string pReference, string pQuery);
   int w(char x,char y);
+   int findMin (int a, int b, int c);
   
   	int **D = creatMatrix(pReference, pQuery) ;
   	int **P = creatMatrix(pReference, pQuery) ;
   	int **Q = creatMatrix(pReference, pQuery) ;
 	InitMat(D,P,Q,gapOpenPenalty,gapExtendPenalty);
 	affiche (pReference, pQuery, D);
-	CalculateMatrixD(D, P, Q,pReference, pQuery);
+	cout << "lignes : " <<endl;
+	cout << "lignes : " <<endl;	
+	cout << "lignes : " <<endl;	
+	cout << "lignes : " <<endl;
+	//CalculateMatrixD(D, P, Q,pReference, pQuery);
 	affiche (pReference, pQuery, P);
 	affiche (pReference, pQuery, Q);
-	affiche (pReference, pQuery, D);
+
+	CalculateAllMatrix(D,P,Q,pReference,pQuery);
+		affiche (pReference, pQuery, D);
+		affiche (pReference, pQuery, P);
+	affiche (pReference, pQuery, Q);
 	
 	return 0;
 }
@@ -76,17 +88,20 @@ int main(int argc, char *argv[])
   	cout << endl;
   }
   
+  
     //gap  Weight
   int CalculateGapWeight(int gapOpenPenalty, int gapExtendPenalty,int k){
   	int w_k=gapOpenPenalty + gapExtendPenalty*k;
    	return w_k;
   }
   
+  
   //distance weight
   int w(char x,char y){
 	  int w = 0;
 	  if (x != y)
 		w = 1;
+	return w;
 	}
   
   int getSizeString(string a){
@@ -96,21 +111,79 @@ int main(int argc, char *argv[])
 	  
   //Matrice Initialisation P Q D
   void InitMat(int** D, int** P, int**Q,int gapOpenPenalty, int gapExtendPenalty){
-  	for(int i = 0; i < 33; i++) {
+   	for(int i = 0; i < 33; i++) {
     	D[i][0] = CalculateGapWeight(gapOpenPenalty, gapExtendPenalty,i);
-    	Q[i][0] = 2000;
+    	Q[i][0] = 99;
   	}
   	for(int j = 0; j < 39; j++) {
   		D[0][j] = CalculateGapWeight(gapOpenPenalty, gapExtendPenalty,j);
-  		P[0][j] = 2000;
+  		P[0][j] = 99;
   	}
   	D[0][0]=0;
   }
   
-  //Calcul Matrix D
-  void CalculateMatrixD(int ** D, int ** P, int ** Q, string pReference, string pQuery){
-	  for (int i=0; i < 33; i++) {
-    	for (int j=0; j < 39; j++)
-			int D[i][j] = min({D[i-1][j-1] + w(pReference[i],pQuery[j]),P[i][j],Q[i][j]});
+
+  
+//find min
+ int findMin (int a, int b, int c){
+	 int m = min(a,b);
+	 int n= min(m,c);
+	 return n;
 	 }
+
+
+
+
+//Calcul Matrix D
+
+
+  int** CalculateMatrixD(int ** D, int ** P, int ** Q, string pReference, string pQuery){
+	  for (int i=1; i < 33; i++) {
+    	for (int j=1; j < 39; j++){
+			 D[i][j] = findMin(D[i-1][j-1] + w(pReference[i],pQuery[j]),P[i][j],Q[i][j]);
+			 cout << w(pReference[i],pQuery[j]) << " ";
+			 }
+ 
+	 }
+	    cout << endl;
+	    return D;
 	}
+
+  int** CalculateMatrixP(int ** D, int ** P, int ** Q, string pReference, string pQuery){
+	  for (int i=1; i < 33; i++) {
+    	for (int j=1; j < 39; j++){
+			 P[i][j] = min(D[i-1][j] + CalculateGapWeight(11, 1, 1),P[i-1][j] + 11);
+			 cout << CalculateGapWeight(11, 1, 1) << " ";
+			 }
+ 
+	 }
+	    cout << endl;
+	    return P;
+	}
+
+  int** CalculateMatrixQ(int ** D, int ** P, int ** Q, string pReference, string pQuery){
+	  for (int i=1; i < 33; i++) {
+    	for (int j=1; j < 39; j++){
+			 Q[i][j] = min(D[i][j-1] + CalculateGapWeight(11, 1, 1),Q[i][j-1] + 11);
+			 cout << CalculateGapWeight(11, 1, 1) << " ";
+			 }
+ 
+	 }
+	    cout << endl;
+	    return Q;
+	}
+
+  void CalculateAllMatrix(int ** D, int ** P, int ** Q, string pReference, string pQuery){
+	InitMat(D, P, Q,11, 1) ;
+	  for (int i=1; i < 33; i++) {
+    	for (int j=1; j < 39; j++){
+			 Q[i][j] = min(D[i][j-1] + CalculateGapWeight(11, 1, 1),Q[i][j-1] + 11);
+			 P[i][j] = min(D[i-1][j] + CalculateGapWeight(11, 1, 1),P[i-1][j] + 11);
+			 D[i][j] = findMin(D[i-1][j-1] + w(pReference[i],pQuery[j]),P[i][j],Q[i][j]);
+			 }
+ 
+	 }
+	    cout << endl;
+	}
+
+  
